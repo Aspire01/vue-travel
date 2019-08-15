1d273b9dfdc22fd839290573346aeb966cc1d4a3
Aspire01 <18253137881@163.com>
2019-08-15 08:32:38 +0800

    modify files

---
+++ src/pages/home/components/Icon.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="icons">
+        Icons
+    </div>
+</template>
+
+<script>
+export default {
+    name:'HomeIcons',
+}
+</script>
+
+<style lang="stylus" scoped>
+    // 实现宽高比2:1的盒子 这是什么原理？
+    .icons
+        height 0
+        overflow hidden
+        padding-bottom 50%
+        background pink    
+</style>
