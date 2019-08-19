de4e9a21a7ae0f4cfe2e99dd1abcdbb384cbabd2
Aspire01 <18253137881@163.com>
2019-08-19 23:54:58 +0800

    index-ajax +ing

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -14,6 +14,7 @@ import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icon'
 import HomeWeekend from './components/Weekend'
 import HomeLike from './components/Like'
+import axios from 'axios'
 export default {
     name:'Home',
     components:{
@@ -22,6 +23,19 @@ export default {
         HomeIcons,
         HomeWeekend,
         HomeLike
+    },
+    methods: {
+        getHomeInfo () {
+            // 路径使用代理实现转发
+            axios.get('/static/mock/index.json')
+                .then(this.getHomeInfoSucc)
+        },
+        getHomeInfoSucc (res) {
+            console.log(res)
+        },
+    },
+    mounted () {
+        this.getHomeInfo();
     }
 }
 </script>
