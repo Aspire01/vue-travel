f722b5a5cfcef1a07a9a04204b0fa635ea169147
Aspire01 <18253137881@163.com>
2019-10-03 17:02:22 +0800

    '123'

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,16 +2,18 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :cities='cities' :hot='hotCityies'></city-list>
+    <city-alphabet :cities='cities'></city-alphabet>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header';
 import CitySearch from './components/Search';
 import CityList from './components/List';
 import CityAlphabet from './components/Alphabet';
+import { log } from 'util';
 export default {
   name: "City",
   components: {
@@ -20,8 +22,29 @@ export default {
     CityList,
     CityAlphabet
   },
+  methods:{
+      getCityInfo(){
+        axios.get('/api/city.json')
+          .then(this.handleSuccess)
+      },
+      handleSuccess(res){
+        console.log(res)
+        res = res.data
+        if(res.ret && res.data){
+          const data = res.data
+          this.cities = data.cities
+          this.hotCityies = data.hotCityies
+        }
+      }
+  },
+  mounted(){
+    this.getCityInfo();
+  },
   data() {
-    return {};
+    return {
+      cities:{},
+      hotCityies:[]
+    };
   }
 };
 </script>
